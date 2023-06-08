4bc4e44ef1857a162def1d05b9f199e13c49255c
changlog <changlog0510@gmail.com>
2023-06-08 13:39:10 +0900

    젠킨스 연결까지

--- src/main/java/test/jenkins/CICDController.java
+++ src/main/java/test/jenkins/CICDController.java
@@ -9,6 +9,7 @@ public class CICDController {
     @GetMapping("/")
     public String test(){
         System.out.println("젠킨스 테스트 1");
+        System.out.println("젠킨스 테스트 : 젠킨스 연결까지 완료");
         return "test";
     }
 }
